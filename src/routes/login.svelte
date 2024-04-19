9ceaa081f534ef3ecee7a008465b02d3bcfa200f
RICTUS <parshintseva-s@mail.ru>
2024-04-20 01:48:11 +0500

    front builded

---
+++ src/routes/login.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { get_root_for_style } from "svelte/internal";
+    import {goto} from "@sapper/app.mjs";
+
+    let email = '', password = ''
+
+    const submit = async() => {
+        await fetch('http://localhost:8000/api/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            credentials: "include",
+            body: JSON.stringify({
+                email,
+                password
+            })
+        });
+
+        await goto('/');
+    }
+</script>
+
+<form on:submit|preventDefault={submit}>
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+  
+    <input bind:value={email} type="email" class="form-control" placeholder="name@example.com">
+
+    <input bind:value={password} type="password" class="form-control" placeholder="Password">
+
+    <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+</form>
---
+++ src/routes/register.svelte
@@ -0,0 +1,31 @@
+<script>
+    import {goto} from "@sapper/app.mjs";
+
+    let name = '', email = '', password = ''
+
+    const submit = async() => {
+        await fetch('http://localhost:8000/api/register', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                name,
+                email,
+                password
+            })
+        });
+
+        await goto('/login');
+    }
+</script>
+
+<form on:submit|preventDefault={submit}>
+    <h1 class="h3 mb-3 fw-normal">Please register</h1>
+  
+    <input bind:value={name} type="name" class="form-control" placeholder="name" required>
+    
+    <input bind:value={email} type="email" class="form-control" placeholder="email" required>
+
+    <input bind:value={password} type="password" class="form-control" placeholder="password" required>
+
+    <button class="btn btn-primary w-100 py-2" type="submit">Submit</button>
+</form>
